0e90583bcc8085848dece64018750845ec3f3e7e
Genta Mochizawa <genta@mochizawa.org>
2025-03-05 23:35:32 +0900

    init

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"os"
+)
+
+func main() {
+	const width = 256
+	const height = 256
+	rect := image.Rect(0, 0, width, height)
+	img := image.NewRGBA(rect)
+	for j := range height {
+		for i := range width {
+			r := float64(i) / (width - 1)
+			g := float64(j) / (height - 1)
+			b := 0.0
+
+			c := color.RGBA{uint8(r * 255.999), uint8(g * 255.999), uint8(b * 255.999), 255}
+			img.Set(i, j, c)
+		}
+	}
+	if err := png.Encode(os.Stdout, img); err != nil {
+		panic(err)
+	}
+}
